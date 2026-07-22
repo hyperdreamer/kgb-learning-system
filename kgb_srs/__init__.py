752d22f5fee8a52fc33fa1517b7ed850591e5a87
hyperdreamer <wenhepku@gmail.com>
2026-07-22 20:05:30 +0800

    release: bump version to 2.2.0

--- kgb_srs/__init__.py
+++ kgb_srs/__init__.py
@@ -3,7 +3,7 @@
 A spaced-repetition learning application with Markdown + MathJax support.
 """
 
-__version__ = "2.1.0"
+__version__ = "2.2.0"
 
 # Non-PyQt modules are safe to import eagerly.
 from .config import (
